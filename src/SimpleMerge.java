335e57a585ab8e11f080c7e9b321a0a0694055d3
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2019-01-31 08:29:13 -0500

    Update SimpleMerge.java

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -4,23 +4,47 @@
  * integers and merges them to return one sorted
  * array of integers.
  */
-public void SimpleMerge {
+public class SimpleMerge {
     
-        public void mergeSort(int[] arr )
-        {
-            int n = arr.length;
-            int[] temp = new int[n];
-            mergeSortHelper(arr, 0 , n-1, temp);
-        }
-
-    public void MergeSortHelper(int[] arr, int left, int right, int[] temp)
+int[] arr3 = (arr1.length + arr2.length);       // combines array 1 and 2 
+    
+   static int sort(int[] arr3, int ind)      //Selection sort used on the array 3 
     {
-        if(left < right)
-        {
-            int mid = (left + right)/2;
-            mergeSortHelper(ar, left,mid, temp);
-            mergeSortHelper(arr, mid+1 , right, temp);
-            merge(arr, left, mid, right, temp);
-        }
+        int minInd = ind;
 
+        for (int i = ind; i <arr.length; i++)
+        {
+            if (arr[i] < arr[minInd])
+            {
+                swap(arr,i,minInd);
+                {
+                minInd = i;
+                }
+
+            }
+
+        }
+        return minInd;
+    }
+    // Swap method to switch vales arr[i] an arr[j]
+    public static void swap(int[] arr, int i , int j)
+    {
+        int x = arr[i];
+        arr[i] = arr[j];
+        arr[j] = x;
+
+    }
+    public static void int minInd(int [] arr, int minInd )
+    {
+        int min = arr[minInd];
+        for (int i = minInd; i<arr.length;i++)
+        {
+            if (arr[i] < minInd){
+                minInd = arr[i];
+            }
+            return minInd;
+        
 }
+}
+    
+    

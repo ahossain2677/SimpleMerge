2b21bf3fb78944ff09b772005acacbdfd5cb77bd
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2019-01-31 08:24:40 -0500

    Update SimpleMerge.java

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -4,12 +4,23 @@
  * integers and merges them to return one sorted
  * array of integers.
  */
-public class SimpleMerge {
-    public static int[] simpleMerge(int[] arr1, int[] arr2) {
-
-    println("Merging:" + arr1 + "and" + arr2);
-    arr3 = (arr1,arr2);
+public void SimpleMerge {
     
-            
-    }
+        public void mergeSort(int[] arr )
+        {
+            int n = arr.length;
+            int[] temp = new int[n];
+            mergeSortHelper(arr, 0 , n-1, temp);
+        }
+
+    public void MergeSortHelper(int[] arr, int left, int right, int[] temp)
+    {
+        if(left < right)
+        {
+            int mid = (left + right)/2;
+            mergeSortHelper(ar, left,mid, temp);
+            mergeSortHelper(arr, mid+1 , right, temp);
+            merge(arr, left, mid, right, temp);
+        }
+
 }
